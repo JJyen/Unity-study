ad0e2b35cb19935e6268a93f32b46a3411c7d65a
JJyen <gdb1632@naver.com>
2024-07-18 00:15:02 +0900

    3.1-2 플레이어 이동

--- yeeun/ShootingGame/Assets/Scripts/PlayerMove.cs
+++ yeeun/ShootingGame/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 5;
 
     // Update is called once per frame
     void Update()
     {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        //print("h: " + h + "v: " + v);
+
+        //Vector3 dir = Vector3.right*h + Vector3.up*v;
+        Vector3 dir = new Vector3(h,v,0);
         
+        //transform.Translate(dir*speed*Time.deltaTime);
+        // =>> P = P0 + vt
+        //transform.position += dir*speed*Time.deltaTime;
+        transform.position = transform.position + dir*speed*Time.deltaTime;
     }
 }
