445d1b16186b3b44c0f7dde70a1ef72ac74f7324
JJyen <gdb1632@naver.com>
2024-07-18 23:57:05 +0900

    점수 추가(2단계)

--- yeeun/ShootingGame/Assets/Scripts/Enemy.cs
+++ yeeun/ShootingGame/Assets/Scripts/Enemy.cs
@@ -56,26 +56,29 @@ public class Enemy : MonoBehaviour
         // 2. ScoreManager 게임 오브젝트에서 ScoreManager 컴포넌트(ScoreManager 스크립트)를 가져옴
         ScoreManager sm = smObject.GetComponent<ScoreManager>();
 
-        // 3. ScoreManager 클래스의 속성에 값 할당
-        sm.currentScore++;
 
-        // 4. 화면에 현재 점수 표시하기
-        sm.currentScoreUI.text = "현재 점수: " + sm.currentScore;
+        // // 3. ScoreManager 클래스의 속성에 값 할당
+        // sm.currentScore++;
+
+        // // 4. 화면에 현재 점수 표시하기
+        // sm.currentScoreUI.text = "현재 점수: " + sm.currentScore;
 
 
-        // [최고 점수 기록하기]
-        // 1. 현재 점수가 최고 점수를 초과했다면
-        if(sm.currentScore > sm.bestScore){
-            // 2. 최고 점수 갱신
-            sm.bestScore = sm.currentScore;
+        // // [최고 점수 기록하기]
+        // // 1. 현재 점수가 최고 점수를 초과했다면
+        // if(sm.currentScore > sm.bestScore){
+        //     // 2. 최고 점수 갱신
+        //     sm.bestScore = sm.currentScore;
 
-            // 3. 최고 점수 UI에 표시
-            sm.bestScoreUI.text = "최고 점수: " + sm.bestScore;
+        //     // 3. 최고 점수 UI에 표시
+        //     sm.bestScoreUI.text = "최고 점수: " + sm.bestScore;
 
-            // [최고 점수 클라이언트에 저장하기]
-            PlayerPrefs.SetInt("Best Score", sm.bestScore); // PlayerPrefs객체: 키-값 형태로 값 저장 
-        }
+        //     // [최고 점수 클라이언트에 저장하기]
+        //     PlayerPrefs.SetInt("Best Score", sm.bestScore); // PlayerPrefs객체: 키-값 형태로 값 저장 
+        // }
 
+        // 33. ScoreManager의 get/set함수 사용
+        sm.SetScore(sm.GetScore() + 1);
 
     }
     private void OnCollisionStay(Collision other){
--- yeeun/ShootingGame/Assets/Scripts/ScoreManager.cs
+++ yeeun/ShootingGame/Assets/Scripts/ScoreManager.cs
@@ -6,9 +6,40 @@ using UnityEngine.UI; // 유니티 UI를 사용하기 위한 네임스페이스
 public class ScoreManager : MonoBehaviour
 {
     public Text currentScoreUI; // 현재 점수 UI
-    public int currentScore; // 현재 점수  
+    private int currentScore; // 현재 점수  
     public Text bestScoreUI; // 최고 점수 UI
-    public int bestScore; // 최고 점수
+    private int bestScore; // 최고 점수
+
+    // currentScore에 값 넣고 화면에 표시하기
+    public void SetScore(int value){
+        currentScore = value;
+        // 3. ScoreManager 클래스의 속성에 값 할당
+        //currentScore++;
+        currentScore = value;
+
+        // 4. 화면에 현재 점수 표시하기
+        currentScoreUI.text = "현재 점수: " + currentScore;
+
+
+        // [최고 점수 기록하기]
+        // 1. 현재 점수가 최고 점수를 초과했다면
+        if(currentScore > bestScore){
+            // 2. 최고 점수 갱신
+            bestScore = currentScore;
+
+            // 3. 최고 점수 UI에 표시
+            bestScoreUI.text = "최고 점수: " + bestScore;
+
+            // [최고 점수 클라이언트에 저장하기]
+            PlayerPrefs.SetInt("Best Score", bestScore); // PlayerPrefs객체: 키-값 형태로 값 저장 
+        }
+
+    }
+
+    // currentScore 값 바꾸기
+    public int GetScore(){
+        return currentScore;
+    }
 
 
     // Start is called before the first frame update
