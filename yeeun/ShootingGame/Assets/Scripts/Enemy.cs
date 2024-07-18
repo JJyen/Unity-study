73088d8c057ad28a55d7e627965fdec1d1339b82
JJyen <gdb1632@naver.com>
2024-07-18 14:59:03 +0900

    3.1-7 플레이어 방향 찾기

--- yeeun/ShootingGame/Assets/Scripts/Enemy.cs
+++ yeeun/ShootingGame/Assets/Scripts/Enemy.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 5; // 이동속도
+    Vector3 dir;
+    void Start()
+    {
+        // 0부터 9까지의 10개의 값 중에 하나를 랜덤으로 가져온다
+        int randValue = UnityEngine.Random.Range(0,10);
+
+        // 랜덤 값이 3보다 작으면(30% 확률로 적이 플레이어 방향으로 이동)
+        if(randValue < 3){
+            // 플레이어를 찾아 타겟으로
+            GameObject target = GameObject.Find("Player"); //괄호 안의 이름을 가진 오브젝트를 찾아서 반환
+
+            // 플레이어 방향 = target - me
+            dir = target.transform.position - transform.position;
+
+            // 방향의 크기는 1
+            dir.Normalize();
+        }else{
+            // 기본 동작
+            dir = Vector3.down;
+        }
+
+    }
 
     void Update()
     {
-        Vector3 dir = Vector3.down; // 방향
-
-        transform.position = dir*speed*Time.deltaTime; // 이동: P = P0 + vt
+        //transform.position = dir*speed*Time.deltaTime; // 이동: P = P0 + vt
+        transform.Translate(dir * speed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision other){
