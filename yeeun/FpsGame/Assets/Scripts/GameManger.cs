93b930bae600eaf162bc08e386d940135d0deb07
JJyen <gdb1632@naver.com>
2024-08-24 11:44:54 +0900

    4.8-2 옵션 기능 구현

--- yeeun/FpsGame/Assets/Scripts/GameManger.cs
+++ yeeun/FpsGame/Assets/Scripts/GameManger.cs
@@ -22,7 +22,8 @@ public class GameManager : MonoBehaviour
     {
         Ready,
         Run,
-        GameOver
+        GameOver,
+        Pause, // 일시정지
     }
 
     // 현재의 게임 상태 변수
@@ -36,6 +37,9 @@ public class GameManager : MonoBehaviour
 
     // PlayerMove 클래스 변수
     PlayerMove player;
+
+    // 옵션 화면 UI 오브젝틑 변수
+    public GameObject gameOption;
 
     void Start()
     {
@@ -92,10 +96,56 @@ public class GameManager : MonoBehaviour
             // 상태 텍스트의 색상을 붉은색으로 한다.
             gameText.color = new Color32(255, 0, 0, 255);
 
+            // 상태 텍스트의 자식 오브젝트의 트랜스폼 컴포넌트를 가져온다.
+            Transform buttons = gameText.transform.GetChild(0);
+
+            // 버튼 오브젝트 활성화
+            buttons.gameObject.SetActive(true);
+
             // 상태를 "게임 오버" 상태로 변경한다.
             gState = GameState.GameOver;
         }
     }
 
+    // 옵션 화면 켜기
+    public void OpenOptionWindow()
+    {
+        // 옵션 창 활성화
+        gameOption.SetActive(true);
+
+        // 게임 속도 0배속으로 전환
+        Time.timeScale = 0f; // timeScale: 게임 전체의 프레임 진행 시간 배율
+
+        // 게임 상태를 일시 정지 상태롤 변경
+        gState = GameState.Pause;
+    }
+
+    public void CloseOptionWindow()
+    {
+        // 옵션 창 비활성화
+        gameOption.SetActive(false);
+
+        // 게임 속도 1배속(정상화)으로 전환
+        Time.timeScale = 1f;
+
+        // 게임 상태를 게임 중 으로 전환
+        gState = GameState.Run;
+    }
     
+    // 다시하기 옵션
+    public void ReStartGame()
+    {
+        // 게임 속도 1배속으로 전환
+        Time.timeScale = 1f;
+
+        // 현재 씬 번호 다시 로드
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // 게임 종료 옵션
+    public void QuitGame()
+    {
+        // 애플리케이션 종료
+        Application.Quit();
+    }
 }
